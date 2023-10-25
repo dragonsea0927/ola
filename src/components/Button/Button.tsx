524231886d81969e2587ba94767485217af5ee4c
dragonsea0927 <dragonsea927@proton.me>
2023-10-25 11:08:23 +0100

    fix(app): Update global css and fix button styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,40 +1,40 @@
-// 'use client'
+'use client'
 
-// import Button from "@mui/material/Button";
-// import { styled } from "@mui/material/styles";
-// import { StyledButtonProps, ButtonProps } from "@/types";
+import { ButtonProps } from "@/types";
 
-// const StyledButton = styled(Button)<StyledButtonProps>(({ theme, width, variant }) => ({
-//   width: variant === 'text' ? width : width,
-//   height: '45px',
-//   padding: '10px 20px',
-//   borderRadius: '50px',
-//   textTransform: 'none',
-//   lineHeight: '16px',
-//   letterSpacing: '0.25px',
-//   boxShadow: 'rgb(209, 217, 230) 5px 5px 15px 0px, rgb(255, 255, 255) -5px -5px 15px 0px',
-//   display: 'flex',
-//   gap: '10px',
-//   color: variant === 'outlined' ? theme.text.primary : variant === 'text' ? theme.palette.secondary.main : theme.white.main,
-//   backgroundColor: variant === 'contained' ? theme.palette.secondary.main : 'transparent',
-//   border: variant === 'outlined' ? `1px solid ${theme.palette.secondary.main}` : 'none',
-//   '&:hover': {
-//     backgroundColor: variant === 'contained' ? theme.white.main : '',
-//     color: theme.palette.secondary.main,
-//     border: variant === 'contained' ? `1px solid ${theme.palette.secondary.main}` : 'none',
-//   },
-// }));
 
-// export default function CustomButton(props: ButtonProps) {
-//   return <StyledButton
-//     variant={props.variant}
-//     color={props.color}
-//     size={props.size}
-//     onClick={props.onClick}
-//     width={props.width}
-//   >
-//     {props.children}
-//   </StyledButton>;
-// }
+export default function CustomButton(props: ButtonProps) {
+  const { variant, color, size, width, children, onClick } = props;
+  const tailwindClasses = `
+  w-${variant === 'text' ? ['50%'] : [width]}
+  h-[45px]
+  px-3 py-5
+  rounded-full
+  leading-4
+  tracking-tighter
+  shadow-lg
+  flex
+  items-center
+  justify-center
+  gap-10
+  text-base
+  md:text-lg
+  text-[var(--ctaText)]
+  ${variant === 'contained' ? `bg-[var(--cta)]` : 'bg-transparent'}
+  ${variant === 'outlined' ? `border border-[var(--primary)]` : 'border-none'}
+  hover:bg-white hover:text-[var(--cta)] hover:border 
+  border-[var(--cta)]'
+`;
+  return <button
+    variant={variant}
+    color={color}
+    size={size}
+    onClick={onClick}
+    width={width}
+    className={tailwindClasses}
+  >
+    {children}
+  </button>;
+}
 
 

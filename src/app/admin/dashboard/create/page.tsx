1950d674897185f56d125a8b0f639bb48e31e699
dragonsea0927 <dragonsea927@proton.me>
2023-11-15 13:38:54 +0100

    feat(app): Update padding for create project page

--- src/app/admin/dashboard/create/page.tsx
+++ src/app/admin/dashboard/create/page.tsx
@@ -4,7 +4,7 @@ import CreateForm from './CreateForm';
 const CreateProjectPage = () => {
   return (
     <>
-      <main className='w-[85%] p-5 mt-[60px] mx-auto bg-[var(--bg)] rounded-lg'>
+      <main className='w-[85%] p-5 my-[60px] mx-auto bg-[var(--bg)] rounded-lg'>
         <h1 className='text-center text-[var(--textColor)] text-5xl'>
           Create Project
         </h1>
